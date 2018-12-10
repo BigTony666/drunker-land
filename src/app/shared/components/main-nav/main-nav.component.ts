dae1482d80ec4841fcf3eecec045c4f82e6f440c
BigTony <kejiezhang666@gmail.com>
2018-12-10 03:26:52 -0500

    fix: fix search bar

--- src/app/shared/components/main-nav/main-nav.component.ts
+++ src/app/shared/components/main-nav/main-nav.component.ts
@@ -1,25 +1,25 @@
-import {Component, EventEmitter, OnInit, Output} from '@angular/core';
-import {WineServiceClient} from '../../../../services/wine.service.client';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { WineServiceClient } from '../../../../services/wine.service.client';
 import { Wine } from '../../../modules/wines/shared/wine.model';
 
 @Component({
   selector: 'app-main-nav',
   templateUrl: './main-nav.component.html',
-  styleUrls: ['./main-nav.component.sass']
+  styleUrls: ['./main-nav.component.scss']
 })
 
 export class MainNavComponent implements OnInit {
   key: string;
-  value: string;
+  inputValue: string;
   @Output() searchWines: EventEmitter<Wine[]> = new EventEmitter();
 
   conditions = [
-    {name: 'Name'},
-    {name: 'Vintage'},
-    {name: 'Color'},
-    {name: 'Region'},
-    {name: 'Appellation'},
-    {name: 'Country'}
+    { name: 'Name' },
+    { name: 'Vintage' },
+    { name: 'Color' },
+    { name: 'Region' },
+    { name: 'Appellation' },
+    { name: 'Country' }
   ];
 
   constructor(
@@ -28,56 +28,64 @@ export class MainNavComponent implements OnInit {
 
   ngOnInit() {
     this.key = '';
-    this.value = '';
+    this.inputValue = '';
   }
 
-  search(key: string, value: string) {
-    switch (key) {
-      case 'Name': {
-        if (value !== '') {
-          this.searchService.getWinesByName(value)
-            .then(response => this.searchWines.emit(response));
+  // onKey(event: KeyboardEvent) {
+  //   this.inputValue = (<HTMLInputElement>event.target).inputValue;
+  // }
+
+  search(key: string) {
+    if (this.inputValue !== '') {
+      this.searchWines.emit(null);
+
+      switch (key) {
+        case 'Name': {
+          if (this.inputValue !== '') {
+            this.searchService.getWinesByName(this.inputValue)
+              .then(response => this.searchWines.emit(response));
+          }
+          break;
         }
-        break;
-      }
-      case 'Vintage': {
-        if (value !== '') {
-          this.searchService.getWinesByVintage(value)
-            .then(response => this.searchWines.emit(response));
+        case 'Vintage': {
+          if (this.inputValue !== '') {
+            this.searchService.getWinesByVintage(this.inputValue)
+              .then(response => this.searchWines.emit(response));
+          }
+          break;
         }
-        break;
-      }
-      case 'Color': {
-        if (value !== '') {
-          this.searchService.getWinesByColor(value)
-            .then(response => this.searchWines.emit(response));
+        case 'Color': {
+          if (this.inputValue !== '') {
+            this.searchService.getWinesByColor(this.inputValue)
+              .then(response => this.searchWines.emit(response));
+          }
+          break;
         }
-        break;
-      }
-      case 'Region': {
-        if (value !== '') {
-          this.searchService.getWinesByRegion(value)
-            .then(response => this.searchWines.emit(response));
+        case 'Region': {
+          if (this.inputValue !== '') {
+            this.searchService.getWinesByRegion(this.inputValue)
+              .then(response => this.searchWines.emit(response));
+          }
+          break;
         }
-        break;
-      }
-      case 'Appellation': {
-        if (value !== '') {
-          this.searchService.getWinesByAppellation(value)
-            .then(response => this.searchWines.emit(response));
+        case 'Appellation': {
+          if (this.inputValue !== '') {
+            this.searchService.getWinesByAppellation(this.inputValue)
+              .then(response => this.searchWines.emit(response));
+          }
+          break;
         }
-        break;
-      }
-      case 'Country': {
-        if (value !== '') {
-          this.searchService.getWinesByCountry(value)
-            .then(response => this.searchWines.emit(response));
+        case 'Country': {
+          if (this.inputValue !== '') {
+            this.searchService.getWinesByCountry(this.inputValue)
+              .then(response => this.searchWines.emit(response));
+          }
+          break;
         }
-        break;
-      }
-      default: {
-        this.searchWines.emit(null);
-        break;
+        default: {
+          this.searchWines.emit([]);
+          break;
+        }
       }
     }
   }
